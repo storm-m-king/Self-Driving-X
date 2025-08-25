28c968bd54889003a3fc68490997f3112fa88042
Storm King <stoking@microsoft.com>
2025-08-25 01:54:37 -0400

    remove redundant environment variable declaration

--- start.bat
+++ start.bat
@@ -38,7 +38,6 @@ REM Run the container with GUI support for Windows/WSLg
 docker run -it --name %CONTAINER% --privileged ^
     -e DISPLAY=host.docker.internal:0.0 ^
     -e QT_X11_NO_MITSHM=1 ^
-    -e GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11 ^
     -v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix ^
     -v /run/desktop/mnt/host/wslg:/mnt/wslg ^
     %IMAGE%
