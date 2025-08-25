6275b29122ba6a19664efb626d416ba5c39822a4
Storm King <stoking@microsoft.com>
2025-08-25 01:50:56 -0400

    fix dockerfile, add start scripts

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,35 @@ FROM osrf/ros:humble-desktop-full
 
 # Install required packages
 RUN apt-get update && \
-    apt-get install -y \
-        software-properties-common \
+    apt-get install -qq -y --no-install-recommends \
         ros-humble-gazebo-* \
         ros-humble-joint-state-publisher-gui \
         ros-humble-rqt-robot-steering \
         locales \
         git \
         nano \
+        build-essential \
+        cmake \
+        libusb-1.0-0-dev \
+        pkg-config \
+        libgtk-3-dev \
+        libglfw3-dev \
+        libgl1-mesa-dev \
+        libglu1-mesa-dev \    
+        libusb-1.0-0 \
+        udev \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        swig \
+        software-properties-common \
         python3-pip \
+    && rm -rf /var/lib/apt/lists/* \
     && locale-gen en_US en_US.UTF-8 \
     && update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 
-ENV LANG en_US.UTF-8
-ENV GAZEBO_RESOURCE_PATH /usr/share/gazebo-11
+ENV LANG=en_US.UTF-8
+ENV GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
 
 # Create workspace and clone limo_ros2 repo
 RUN mkdir -p /limo_ws/src && \
@@ -25,15 +40,24 @@ RUN mkdir -p /limo_ws/src && \
               /limo_ws/src/limo_ros2/limo_car/src \
               /limo_ws/src/limo_ros2/limo_car/worlds
 
+WORKDIR /limo_ws
+
+# Clone the YDLidar-SDK 
+RUN git clone https://github.com/YDLIDAR/YDLidar-SDK.git &&\
+    mkdir -p YDLidar-SDK/build && \
+    cd YDLidar-SDK/build &&\
+    cmake ..&&\
+    make &&\
+    make install &&\
+    cd .. &&\
+    pip install . &&\
+    cd .. && rm -r YDLidar-SDK 
+
 # Copy patch files from local repo into the image
-COPY Self-Driving-X/patches/ackermann_with_sensor.xacro /limo_ws/src/limo_ros2/limo_car/gazebo/ackermann_with_sensor.xacro
-COPY Self-Driving-X/patches/empty_world.sdf /limo_ws/src/limo_ros2/limo_car/worlds/empty_world.sdf
-COPY Self-Driving-X/patches/ackermann_gazebo.launch.py /limo_ws/src/limo_ros2/limo_car/launch/ackermann_gazebo.launch.py
+COPY /patches/ackermann_with_sensor.xacro /limo_ws/src/limo_ros2/limo_car/gazebo/ackermann_with_sensor.xacro
+COPY /patches/empty_world.sdf /limo_ws/src/limo_ros2/limo_car/worlds/empty_world.sdf
+COPY /patches/ackermann_gazebo.launch.py /limo_ws/src/limo_ros2/limo_car/launch/ackermann_gazebo.launch.py
 
 # Set up ROS 2 environment
 RUN echo "source /opt/ros/humble/setup.bash" >> /root/.bashrc
-RUN . /opt/ros/humble/setup.bash && colcon build
-
-WORKDIR /limo_ws
-
 CMD ["/bin/bash"]
---
+++ start.bat
@@ -0,0 +1,211 @@
+@echo off
+cls
+setlocal enabledelayedexpansion
+
+call :setESC
+
+set useColor=true
+
+set color_primary=Blue
+set color_mute=Gray
+set color_info=Yellow
+set color_success=Green
+set color_warn=DarkYellow
+set color_error=Red
+
+REM Set image and container names
+set IMAGE=osrf/ros:humble-desktop-full
+set CONTAINER=limo_dev
+
+REM Check if a container with the same name already exists
+docker ps -a --filter "name=^/%CONTAINER%$" --format "{{.Names}}" | findstr /i "%CONTAINER%" >nul
+if %errorlevel%==0 (
+    call :WriteLine "WARNING: A container named '%CONTAINER%' already exists. Will not create a new one." "DarkYellow"
+    call :WriteLine "If you want to remove it and start fresh, run:" "Yellow"
+    call :WriteLine "    docker rm -f %CONTAINER%" "Yellow"
+    call :WriteLine "Connecting to the existing container..." "Yellow"
+    docker exec -it %CONTAINER% /bin/bash
+    endlocal
+    exit /b 1
+)
+
+call :WriteLine "Building the docker image '%IMAGE%' with name '%CONTAINER%'" "Green"
+
+REM Build the Docker image from the local Dockerfile
+docker build -t %IMAGE% .
+
+REM Run the container with GUI support for Windows/WSLg
+docker run -it --name %CONTAINER% --privileged ^
+    -e DISPLAY=host.docker.internal:0.0 ^
+    -e QT_X11_NO_MITSHM=1 ^
+    -e GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11 ^
+    -v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix ^
+    -v /run/desktop/mnt/host/wslg:/mnt/wslg ^
+    %IMAGE%
+endlocal
+
+
+:: sub-routines
+
+:: Sets up the ESC string for use later in this script
+:setESC
+    for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+      set ESC=%%b
+      exit /B 0
+    )
+    exit /B 0
+
+
+:: Sets the name of a color that will providing a contrasting foreground
+:: color for the given background color.
+::
+:: string background color name. 
+:: on return, contrastForeground will be set
+:setContrastForeground
+
+    set background=%~1
+
+    if "!background!"=="" background=Black
+
+    if /i "!background!"=="Black"       set contrastForeground=White
+    if /i "!background!"=="DarkRed"     set contrastForeground=White
+    if /i "!background!"=="DarkGreen"   set contrastForeground=White
+    if /i "!background!"=="DarkYellow"  set contrastForeground=White
+    if /i "!background!"=="DarkBlue"    set contrastForeground=White
+    if /i "!background!"=="DarkMagenta" set contrastForeground=White
+    if /i "!background!"=="DarkCyan"    set contrastForeground=White
+    if /i "!background!"=="Gray"        set contrastForeground=Black
+    if /i "!background!"=="DarkGray"    set contrastForeground=White
+    if /i "!background!"=="Red"         set contrastForeground=White
+    if /i "!background!"=="Green"       set contrastForeground=White
+    if /i "!background!"=="Yellow"      set contrastForeground=Black
+    if /i "!background!"=="Blue"        set contrastForeground=White
+    if /i "!background!"=="Magenta"     set contrastForeground=White
+    if /i "!background!"=="Cyan"        set contrastForeground=Black
+    if /i "!background!"=="White"       set contrastForeground=Black
+
+    exit /B 0
+
+
+:: Sets the currentColor global for the given foreground/background colors
+:: currentColor must be output to the terminal before outputing text in
+:: order to generate a colored output.
+::
+:: string foreground color name. Optional if no background provided.
+::        Defaults to "White"
+:: string background color name.  Optional. Defaults to Black.
+:setColor
+
+    REM If you want to get a little fancy then you can also try
+    REM  - %ESC%[4m - Underline
+    REM  - %ESC%[7m - Inverse
+
+    set foreground=%~1
+    set background=%~2
+
+    if "!foreground!"=="" set foreground=White
+    if /i "!foreground!"=="Default" set foreground=White
+    if "!background!"=="" set background=Black
+    if /i "!background!"=="Default" set background=Black
+
+    if "!ESC!"=="" call :setESC
+
+    if /i "!foreground!"=="Contrast" (
+		call :setContrastForeground !background!
+		set foreground=!contrastForeground!
+	)
+
+    set currentColor=
+
+    REM Foreground Colours
+    if /i "!foreground!"=="Black"       set currentColor=!ESC![30m
+    if /i "!foreground!"=="DarkRed"     set currentColor=!ESC![31m
+    if /i "!foreground!"=="DarkGreen"   set currentColor=!ESC![32m
+    if /i "!foreground!"=="DarkYellow"  set currentColor=!ESC![33m
+    if /i "!foreground!"=="DarkBlue"    set currentColor=!ESC![34m
+    if /i "!foreground!"=="DarkMagenta" set currentColor=!ESC![35m
+    if /i "!foreground!"=="DarkCyan"    set currentColor=!ESC![36m
+    if /i "!foreground!"=="Gray"        set currentColor=!ESC![37m
+    if /i "!foreground!"=="DarkGray"    set currentColor=!ESC![90m
+    if /i "!foreground!"=="Red"         set currentColor=!ESC![91m
+    if /i "!foreground!"=="Green"       set currentColor=!ESC![92m
+    if /i "!foreground!"=="Yellow"      set currentColor=!ESC![93m
+    if /i "!foreground!"=="Blue"        set currentColor=!ESC![94m
+    if /i "!foreground!"=="Magenta"     set currentColor=!ESC![95m
+    if /i "!foreground!"=="Cyan"        set currentColor=!ESC![96m
+    if /i "!foreground!"=="White"       set currentColor=!ESC![97m
+    if "!currentColor!"=="" set currentColor=!ESC![97m
+	
+    if /i "!background!"=="Black"       set currentColor=!currentColor!!ESC![40m
+    if /i "!background!"=="DarkRed"     set currentColor=!currentColor!!ESC![41m
+    if /i "!background!"=="DarkGreen"   set currentColor=!currentColor!!ESC![42m
+    if /i "!background!"=="DarkYellow"  set currentColor=!currentColor!!ESC![43m
+    if /i "!background!"=="DarkBlue"    set currentColor=!currentColor!!ESC![44m
+    if /i "!background!"=="DarkMagenta" set currentColor=!currentColor!!ESC![45m
+    if /i "!background!"=="DarkCyan"    set currentColor=!currentColor!!ESC![46m
+    if /i "!background!"=="Gray"        set currentColor=!currentColor!!ESC![47m
+    if /i "!background!"=="DarkGray"    set currentColor=!currentColor!!ESC![100m
+    if /i "!background!"=="Red"         set currentColor=!currentColor!!ESC![101m
+    if /i "!background!"=="Green"       set currentColor=!currentColor!!ESC![102m
+    if /i "!background!"=="Yellow"      set currentColor=!currentColor!!ESC![103m
+    if /i "!background!"=="Blue"        set currentColor=!currentColor!!ESC![104m
+    if /i "!background!"=="Magenta"     set currentColor=!currentColor!!ESC![105m
+    if /i "!background!"=="Cyan"        set currentColor=!currentColor!!ESC![106m
+    if /i "!background!"=="White"       set currentColor=!currentColor!!ESC![107m
+
+    exit /B 0
+
+:: Outputs a line, including linefeed, to the terminal using the given foreground / background
+:: colors 
+::
+:: string The text to output. Optional if no foreground provided. Default is just a line feed.
+:: string Foreground color name. Optional if no background provided. Defaults to "White"
+:: string Background color name. Optional. Defaults to "Black"
+:WriteLine
+    SetLocal EnableDelayedExpansion
+	
+    if "!ESC!"=="" call :setESC	
+    set resetColor=!ESC![0m
+
+    set str=%~1
+
+    if "!str!"=="" (
+        echo:
+        exit /b 0
+    )
+    if "!str: =!"=="" (
+        echo:
+        exit /b 0
+    )
+
+    if /i "%useColor%"=="true" (
+        call :setColor %2 %3
+        echo !currentColor!!str!!resetColor!
+    ) else (
+        echo !str!
+    )
+    exit /b 0
+
+:: Outputs a line without a linefeed to the terminal using the given foreground / background colors 
+::
+:: string The text to output. Optional if no foreground provided. Default is just a line feed.
+:: string Foreground color name. Optional if no background provided. Defaults to "White"
+:: string Background color name. Optional. Defaults to "Black"
+:Write
+    SetLocal EnableDelayedExpansion
+	
+    if "!ESC!"=="" call :setESC
+    set resetColor=!ESC![0m
+
+    set str=%~1
+
+    if "!str!"=="" exit /b 0
+    if "!str: =!"=="" exit /b 0
+
+    if /i "%useColor%"=="true" (
+        call :setColor %2 %3
+        <NUL set /p =!currentColor!!str!!resetColor!
+    ) else (
+        <NUL set /p =!str!
+    )
+    exit /b 0
